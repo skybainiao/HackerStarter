041454b77c7ec2edc4b43497120a026d28de36a2
Junjie Chen <chen2468830035@gmail.com>
2022-10-09 23:27:08 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+let todolist = document.getElementsByTagName('ul')[0];
+let inputEle = document.getElementById('content');
+let button = document.getElementsByTagName('button')[0];
+let login = false;
+
+
+function addNew(content){
+  let item = document.createElement('li');
+  item.innerHTML = content;
+  todolist.appendChild(item);
+}
+
+
+inputEle.addEventListener('keyup',(evevt) =>{
+  console.log(evevt);
+  if(evevt.key === 'Enter'){
+    if (inputEle.value==='login') {
+      login = true;
+      addNew(inputEle.value+"......");
+      addNew("Login Success!");
+      inputEle.value='';
+    }
+    else if (inputEle.value==='Wanzi Ma' && login === true) {
+      addNew(inputEle.value+'是傻逼');
+      inputEle.value='';
+    }
+    else{
+      addNew(inputEle.value);
+      inputEle.value='';
+    }
+
+  }
+})
