767137cef8e357aed9f36b0a4fe1ca2b5d5757c5
Junjie Chen <chen2468830035@gmail.com>
2022-10-13 04:56:24 +0200

    update

--- main.js
+++ main.js
@@ -1,41 +1,103 @@
-let todolist = document.getElementsByTagName('ul')[0];
+let todoList = document.getElementsByTagName('ul')[0];
 let inputEle = document.getElementById('content');
 let login = false;
+let isRegister = false;
 
 
-function cmd(content){
+function cmd(){
   let item = document.createElement('li');
-  item.innerHTML = "Execute : "+content;
-  todolist.appendChild(item);
+  item.innerHTML = "Execute : "+inputEle.value;
+  inputEle.value='';
+  todoList.appendChild(item);
 }
 
 function comment(content){
   let item = document.createElement('li');
   item.innerHTML = content;
   item.style.color="white";
-  todolist.appendChild(item);
+  todoList.appendChild(item);
 }
 
+function clearInput() {
+  inputEle.value='';
+}
 
 inputEle.addEventListener('keyup',(evevt) =>{
-  console.log(evevt);
   if(evevt.key === 'Enter'){
-    if (inputEle.value==='login') {
+
+    if (inputEle.value==='/login') {
       login = true;
-      cmd(inputEle.value+"......");
+      cmd();
       comment("Login Success!");
-      inputEle.value='';
+    
     }
-    else if (inputEle.value==='Wanzi Ma' && login === true) {
-      cmd('Wanzi Ma');
+
+    else if (inputEle.value==='/register') {
+      cmd();
+      isRegister = true;
+      comment("Enter username and password in this format(username!password!passwordAgain)");
+    }
+
+    else if (inputEle.value==='/test') {
+      cmd();
+      var str = "aaabbbc!ccddde!eefff";
+      //str = str.match(/!(\S*)!/)[1];
+      str = str.match(/(\S*)!/)[1];
+      str = str.match(/(\S*)!/)[1];
+
+      comment(str);
+    }
+
+    else if (isRegister===true) {
+      comment("Registering...")
+
+      var str = inputEle.value;
+      var username = str.match(/(\S*)!/)[1];
+      username = username.match(/(\S*)!/)[1];
+      var password = str.match(/!(\S*)!/)[1];
+      var passwordAgain = str.match(/!(\S*)/)[1];
+      passwordAgain = passwordAgain.match(/!(\S*)/)[1];
+
+
+      if (password=passwordAgain) {
+        var user = {
+          "username":username,
+          "password":password
+         }
+         $.ajax({
+           type:'post',
+           url:'http://hacker/add',
+           data:JSON.stringify(user), 
+           dataType:"text",
+           contentType:"application/json", 
+           success(data){
+             comment("Success")
+             clearInput();
+           },
+           error(err){
+            console.log(err);
+            comment(err);
+          }
+         });
+      }
+      else{
+        comment("invalidPassword")
+      }
+
+      isRegister = false;
+    }
+
+    else if (inputEle.value==='/Wanzi Ma' && login === true) {
+      cmd();
       comment(inputEle.value+'是傻逼');
-      inputEle.value='';
+  
     }
+
     else if (inputEle.value==='get') {
-      cmd(inputEle.value);
-      inputEle.value='';
+      cmd();
+  
       $.ajax({
-        url:'https://www.hackerstarters.com/users',
+        url:'http://localhost:8080/users',
         type:'get',
         dataType:'json',
         success(data){
@@ -50,9 +112,36 @@ inputEle.addEventListener('keyup',(evevt) =>{
         }
       })
     }
+
+    else if (inputEle.value==='post') {
+      cmd();
+      var user = {
+        "username":"chen",
+        "password":"1411"
+       }
+       $.ajax({
+         type:'post',
+         url:'http://localhost:8080/add',
+         data:JSON.stringify(user), 
+         dataType:"text",
+         contentType:"application/json", 
+         success(data){
+           comment("Success")
+         },
+         error(err){
+          console.log(err);
+          comment(err);
+        }
+       });
+  
+    }
+
+    
+
     else{
-      cmd(inputEle.value);
-      inputEle.value='';
+      cmd();
+    
+
     }
 
 
