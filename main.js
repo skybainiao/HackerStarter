9990caebce8097fa84c53150dc2c74b6798c189e
Junjie Chen <chen2468830035@gmail.com>
2022-10-15 22:18:02 +0200

    wadda

--- main.js
+++ main.js
@@ -4,12 +4,34 @@ let login = false;
 let isRegister = false;
 let isName = false;
 let key = "EVQeP83jOOGNNvajzZEeQLSXBNTO4d62qrSxQRU1";
+let docToPDFKey = "538a700e-f896-4246-b324-232a9e19d26f";
 
 
 
 window.onload = function(){
   comment("Enter /help get commands");
 }
+
+var defaultClient = cloudmersiveValidateApiClient.ApiClient.instance;
+
+// Configure API key authorization: Apikey
+var Apikey = defaultClient.authentications['Apikey'];
+Apikey.apiKey = "YOUR_API_KEY_HERE"
+
+var api = new cloudmersiveValidateApiClient.DomainApi()
+
+var domain = "cloudmersive.com"; // {String} Domain name to check, for example \"cloudmersive.com\".  The input is a string so be sure to enclose it in double-quotes.
+
+
+var callback = function (error, data, response) {
+    if (error) {
+        console.error(error);
+    } else {
+        console.log('API called successfully. Returned data: ' + data);
+    }
+};
+
+api.domainCheck(domain, callback);
 
 function cmd(){
   let item = document.createElement('li');
