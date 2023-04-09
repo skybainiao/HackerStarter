72a4d88ec1b36c710a0c480e24d6c1e4ec369daa
chen <chen2468830035@gmail.com>
2023-04-09 13:35:11 +0200

    apr9

---
+++ .history/newJS/home_20230409132551.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+        
+          
+    const menuButton = document.getElementById('menuButton');
+    const navbarMenu = document.getElementById('navbarMenu');
+
+      menuButton.addEventListener('click', function() {
+          navbarMenu.classList.toggle('hidden');
+      });
+
+
+    
+});
